2b95ad4b4a52d259e3bf82c9decdbeb262b86347
Adan Asim <67644268+Adan-Asim@users.noreply.github.com>
2021-07-20 00:10:43 +0500

    Add files via upload
    Developed a calculator with the basic functionalities i.e., add, subtract, multiply and divide. 
    
    Each of the functionality is made as an individual class in a separate package. That is,
     Addition functionality is in a package: calculator.addition
     Subtraction functionality is in a package: calculator.subtract
     Multiplication functionality is in a package: calculator.multiply
     Division functionality is in a package: calculator.division
    A Driver class is in a separate package, imported all the packages in it, and shown the execution of the program.

---
+++ Calculator/addition/Addition.java
@@ -0,0 +1,9 @@
+package Calculator.addition;
+
+public class Addition
+{
+	public static float addition(float a, float b)
+	{
+		return a + b;
+	}
+}
---
+++ Calculator/division/Division.java
@@ -0,0 +1,9 @@
+package Calculator.division;
+
+public class Division
+{
+	public static float division(float a, float b)
+	{
+		return a/b;
+	}
+}
